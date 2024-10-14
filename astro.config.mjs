4d6b1f648a4d262ba4486188851878b954779f7f
mahyar-jahaninasab <jahaninasabmahyar@gmail.com>
2024-10-14 10:25:15 +0330

    address

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://mahyar-jahaninasab.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
